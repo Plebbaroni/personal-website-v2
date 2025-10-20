9d218b3695d58230fad2a0906ea1c906d5c1bee5
Jared Lucas Amistad Schulz <gorillacyborgz@gmail.com>
2025-10-21 01:34:46 +1100

    explicitly change viewport width

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,7 @@ export const metadata: Metadata = {
   icons: {
     icon: "/icons/favicon.png",
   },
+  viewport: "width=device-width, initial-scale=1.0",
 };
 export default function RootLayout({
   children,
