ea5e3be771d2bf22c743575be14cbcff07f896ea
Jared Lucas Amistad Schulz <gorillacyborgz@gmail.com>
2025-10-21 02:04:53 +1100

    onelast

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,33 +4,30 @@
 import MainPage from "./MainPage/MainPage";
 import classes from "./page.module.css";
 import { motion } from "framer-motion";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./globals.css";
 import ClientOnly from "./components/ClientOnly/ClientOnly";
 
 export default function Home() {
-  useEffect(() => {
-      const setVh = () => {
-        document.documentElement.style.setProperty("--real-vh", `${window.innerHeight}px`);
-      };
-
-      setVh();
-      window.addEventListener("resize", setVh);
-      return () => window.removeEventListener("resize", setVh);
-    }, []);
+  const [height, setHeight] = useState<number | null>(null);
+   useEffect(() => {
+    const updateHeight = () => setHeight(window.innerHeight);
+    updateHeight();
+    window.addEventListener("resize", updateHeight);
+    return () => window.removeEventListener("resize", updateHeight);
+  }, []);
+  if (height === null) return null;
 
   return (
-    <ClientOnly>
-      <motion.div
-              className={classes.wrapper}
-              initial={{ opacity: 0, y: 5 }}
-              animate={{ opacity: 1, y: 0 }}
-              transition={{ duration: 0.3, ease: "easeOut" }}
-      >
-        <div className={classes.fullheight}>
-          <MainPage/>
-        </div>
-      </motion.div>
-    </ClientOnly>
+    <motion.div
+            className={classes.wrapper}
+            initial={{ opacity: 0, y: 5 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.3, ease: "easeOut" }}
+    >
+      <div className={classes.fullheight}>
+        <MainPage/>
+      </div>
+    </motion.div>
   );
 }
