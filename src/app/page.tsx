e8fb3d9a55dc623629a647559dd4737d498cf115
Jared Lucas Amistad Schulz <gorillacyborgz@gmail.com>
2025-10-21 02:11:39 +1100

    please

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,26 +8,14 @@ import { useEffect, useState } from "react";
 import "./globals.css";
 
 export default function Home() {
-  const [height, setHeight] = useState<number | null>(null);
-   useEffect(() => {
-    const updateHeight = () => setHeight(window.innerHeight);
-    updateHeight();
-    window.addEventListener("resize", updateHeight);
-    return () => window.removeEventListener("resize", updateHeight);
-  }, []);
-  if (height === null) return null;
-
   return (
     <motion.div
             className={classes.wrapper}
-            style={{height}}
             initial={{ opacity: 0, y: 5 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.3, ease: "easeOut" }}
     >
-      <div className={classes.fullheight}>
-        <MainPage/>
-      </div>
+      <MainPage/>
     </motion.div>
   );
 }
