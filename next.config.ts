9230e93d29b51e9eaaeb79ac7d11fc9185bdf750
Jared Lucas Amistad Schulz <gorillacyborgz@gmail.com>
2025-12-31 17:36:12 +1100

    pls compile friend

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: false,
   productionBrowserSourceMaps: true, // so you can debug
-  swcMinify: false,                  // prevents aggressive code stripping
   webpack(config, { dev }) {
     // Optional: disable minification altogether
     if (!dev) {
