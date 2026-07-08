3c32860ba0b8245e1af421fa5134a6ea34c9ce74
Jared Lucas Amistad Schulz <gorillacyborgz@gmail.com>
2026-07-09 01:31:22 +1000

    code of decree

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 const geistSans = Geist({
@@ -17,7 +17,11 @@ export const metadata: Metadata = {
   icons: {
     icon: "/icons/favicon.png",
   },
-  viewport: "width=device-width, initial-scale=1.0",
+};
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
 };
 export default function RootLayout({
   children,
